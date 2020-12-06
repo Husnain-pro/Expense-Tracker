f925c34f468281d6ef7d2d16db3c521c1f2c23c8
Husnain-pro <mlhlk1212@gmail.com>
2020-12-06 22:51:11 +0500

    Expense_tracker completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import AddTransaction from './components/AddTransaction';
+import Balance from './components/Balance';
+import Header from './components/Header';
+import Expense from './components/incomeExpenses';
+import TransactionList from './components/TransactionList';
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header></Header>
+			<GlobalProvider>
+				<div className="container">
+					<Balance />
+					<Expense />
+					<TransactionList />
+					<AddTransaction />
+				</div>
+			</GlobalProvider>
+		</div>
+	);
 }
 
 export default App;
